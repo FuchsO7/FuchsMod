da0caaeff472affe5fb0cfa4ca2b5de00115e006
FuchsO7 <mc.fuchs07@gmail.com>
2026-06-20 23:09:30 +0200

    Remove redundant import

--- src/main/java/de/fuchsmod/FuchsMod.java
+++ src/main/java/de/fuchsmod/FuchsMod.java
@@ -5,7 +5,6 @@ import de.fuchsmod.config.FuchsModConfigManager;
 import de.fuchsmod.features.*;
 import net.fabricmc.api.ClientModInitializer;
 
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
