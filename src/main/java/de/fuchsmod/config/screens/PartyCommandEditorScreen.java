176022d5886b3ac564df265e0d5d2913cd469d9b
FuchsO7 <mc.fuchs07@gmail.com>
2026-07-05 11:38:21 +0200

    Fixed Party Command Editor Screen breaking on Screen resize Bumped Mod Version to 1.1.1

--- src/main/java/de/fuchsmod/config/screens/PartyCommandEditorScreen.java
+++ src/main/java/de/fuchsmod/config/screens/PartyCommandEditorScreen.java
@@ -13,7 +13,6 @@ import net.minecraft.network.chat.Component;
 public class PartyCommandEditorScreen extends Screen {
     private final Screen parent;
     private final Option<PartyCommandRecord> option;
-    private final HeaderAndFooterLayout layout = new HeaderAndFooterLayout(this);
 
     private boolean publicScope = false;
     private boolean partyScope = false;
@@ -27,9 +26,9 @@ public class PartyCommandEditorScreen extends Screen {
     private static final int DEFAULT_WIDTH = 60;
     private static final int DEFAULT_HEIGHT = 20;
 
-    private final StringWidget scopesLabel = new StringWidget(DEFAULT_WIDTH, DEFAULT_HEIGHT, Component.literal("Scopes"), font);
     private final StringWidget triggerLabel = new StringWidget(DEFAULT_WIDTH, DEFAULT_HEIGHT, Component.literal("Trigger:"), font);
     private final StringWidget commandLabel = new StringWidget(DEFAULT_WIDTH, DEFAULT_HEIGHT, Component.literal("Executes:"), font);
+    private final StringWidget scopesLabel = new StringWidget(DEFAULT_WIDTH, DEFAULT_HEIGHT, Component.literal("Scopes:"), font);
     private EditBox triggerText;
     private EditBox commandText;
     private CycleButton<Boolean> publicScopeButton;
@@ -47,99 +46,97 @@ public class PartyCommandEditorScreen extends Screen {
             this.publicScope = true;
         if ((value.scopes() >> 1) % 2 == 1)
             this.partyScope = true;
-        if ((value.scopes() >> 2) % 2 == 1 )
+        if ((value.scopes() >> 2) % 2 == 1)
             this.guildScope = true;
-        if ((value.scopes() >> 3) % 2 == 1 )
+        if ((value.scopes() >> 3) % 2 == 1)
             this.officerScope = true;
         this.trigger = value.trigger();
         this.command = value.command();
         this.replacementFunction = value.replacementFunction();
     }
 
-    public PartyCommandRecord exportSettings() {
+    public PartyCommandRecord getSettings() {
         int scopes = 0;
-        if (publicScope)
+        if (this.publicScope)
             scopes += 1;
-        if (partyScope)
+        if (this.partyScope)
             scopes += 2;
-        if (guildScope)
+        if (this.guildScope)
             scopes += 4;
-        if (officerScope)
+        if (this.officerScope)
             scopes += 8;
 
-        return new PartyCommandRecord(scopes, triggerText.getValue(), commandText.getValue(), replacementFunction);
+        return new PartyCommandRecord(scopes, this.triggerText.getValue(), this.commandText.getValue(), this.replacementFunction);
     }
 
     @Override
     public void onClose() {
-        option.requestSet(exportSettings());
-        minecraft.gui.setScreen(parent);
+        PartyCommandRecord newSettings = getSettings();
+        if (!newSettings.equals(this.option.pendingValue()))
+            this.option.requestSet(getSettings());
+        minecraft.gui.setScreen(this.parent);
     }
 
     @Override
     protected void init() {
-        super.init();
+        HeaderAndFooterLayout layout = new HeaderAndFooterLayout(this);
         layout.addTitleHeader(getTitle(), font);
         LinearLayout footerLayout = layout.addToFooter(LinearLayout.horizontal().spacing(DEFAULT_SPACING));
         footerLayout.addChild(Button.builder(CommonComponents.GUI_DONE, _ -> onClose()).build());
 
         LinearLayout content = layout.addToContents(LinearLayout.vertical().spacing(DEFAULT_SPACING));
-        content.defaultCellSetting().alignHorizontallyCenter();
 
         LinearLayout triggerRow = content.addChild(LinearLayout.horizontal().spacing(DEFAULT_SPACING));
-        triggerRow.defaultCellSetting().alignHorizontallyCenter().alignVerticallyMiddle();
-        triggerRow.addChild(triggerLabel);
-        triggerText = new EditBox(font, Component.literal(""));
-        triggerText.setMaxLength(255);
-        triggerText.setValue(trigger);
-        triggerText.setWidth(width / 2);
-        triggerText.setTooltip(Tooltip.create(Component.literal("The command will be executed if this string is found in chat.")));
-        triggerRow.addChild(triggerText);
+        triggerRow.addChild(this.triggerLabel);
+        this.triggerText = new EditBox(font, Component.literal(""));
+        this.triggerText.setMaxLength(255);
+        this.triggerText.setValue(this.trigger);
+        this.triggerText.setWidth(width / 2);
+        this.triggerText.setTooltip(Tooltip.create(Component.literal("The command will be executed if this string is found in chat.")));
+        triggerRow.addChild(this.triggerText);
 
         LinearLayout commandRow = content.addChild(LinearLayout.horizontal().spacing(DEFAULT_SPACING));
-        commandRow.defaultCellSetting().alignHorizontallyCenter().alignVerticallyMiddle();
-        commandRow.addChild(commandLabel);
-        commandText = new EditBox(font, Component.literal(""));
-        commandText.setMaxLength(255);
-        commandText.setValue(command);
-        commandText.setWidth(width / 2);
-        commandText.setTooltip(Tooltip.create(Component.literal("This is the command to be executed. " +
+        commandRow.addChild(this.commandLabel);
+        this.commandText = new EditBox(font, Component.literal(""));
+        this.commandText.setMaxLength(255);
+        this.commandText.setValue(this.command);
+        this.commandText.setWidth(width / 2);
+        this.commandText.setTooltip(Tooltip.create(Component.literal("This is the command to be executed. " +
                 "The {player} placeholder will be replaced with the senders name. " +
                 "Use the {args[i]} placeholders to add arguments given after the trigger.")));
-        commandRow.addChild(commandText);
+        commandRow.addChild(this.commandText);
 
         LinearLayout scopesRow = content.addChild(LinearLayout.horizontal().spacing(DEFAULT_SPACING));
-        scopesRow.defaultCellSetting().alignHorizontallyCenter().alignVerticallyMiddle();
-        scopesRow.addChild(scopesLabel);
+        scopesRow.defaultCellSetting().alignVerticallyMiddle();
+        scopesRow.addChild(this.scopesLabel);
         LinearLayout scopesColumn = scopesRow.addChild(LinearLayout.vertical().spacing(DEFAULT_SPACING));
-        scopesColumn.defaultCellSetting().alignHorizontallyCenter().alignVerticallyMiddle();
-        publicScopeButton = CycleButton.onOffBuilder(publicScope).create(Component.literal("Public"),
-                (button, value) -> publicScope = value);
-        publicScopeButton.setWidth(width / 2);
-        scopesColumn.addChild(publicScopeButton);
-        partyScopeButton = CycleButton.onOffBuilder(partyScope).create(Component.literal("Party"),
-                (button, value) -> partyScope = value);
-        partyScopeButton.setWidth(width / 2);
-        scopesColumn.addChild(partyScopeButton);
-        guildScopeButton = CycleButton.onOffBuilder(guildScope).create(Component.literal("Guild"),
-                (button, value) -> guildScope = value);
-        guildScopeButton.setWidth(width / 2);
-        scopesColumn.addChild(guildScopeButton);
-        officerScopeButton = CycleButton.onOffBuilder(officerScope).create(Component.literal("Officer"),
-                (button, value) -> officerScope = value);
-        officerScopeButton.setWidth(width / 2);
-        scopesColumn.addChild(officerScopeButton);
+        this.publicScopeButton = CycleButton.onOffBuilder(this.publicScope).create(Component.literal("Public"),
+                (button, value) -> this.publicScope = value);
+        this.publicScopeButton.setWidth(width / 2);
+        scopesColumn.addChild(this.publicScopeButton);
+        this.partyScopeButton = CycleButton.onOffBuilder(this.partyScope).create(Component.literal("Party"),
+                (button, value) -> this.partyScope = value);
+        this.partyScopeButton.setWidth(width / 2);
+        scopesColumn.addChild(this.partyScopeButton);
+        this.guildScopeButton = CycleButton.onOffBuilder(this.guildScope).create(Component.literal("Guild"),
+                (button, value) -> this.guildScope = value);
+        this.guildScopeButton.setWidth(width / 2);
+        scopesColumn.addChild(this.guildScopeButton);
+        this.officerScopeButton = CycleButton.onOffBuilder(this.officerScope).create(Component.literal("Officer"),
+                (button, value) -> this.officerScope = value);
+        this.officerScopeButton.setWidth(width / 2);
+        scopesColumn.addChild(this.officerScopeButton);
 
         LinearLayout specialFunctionRow = content.addChild(LinearLayout.horizontal().spacing(DEFAULT_SPACING));
-        replacementFunctionButton = CycleButton.builder(Component::literal, replacementFunction)
+        this.replacementFunctionButton = CycleButton.builder(Component::literal, this.replacementFunction)
                 .withValues(PartyCommandUtils.replacementCommands.keySet())
                 .create(Component.literal("Special Replacement Function"),
-                        (button, value) -> replacementFunction = value);
-        replacementFunctionButton.setWidth(width / 2 + DEFAULT_WIDTH + DEFAULT_SPACING);
-        replacementFunctionButton.setTooltip(Tooltip.create(Component.literal("On execution, the placeholder {function} will be replaced by this functions return value.")));
-        specialFunctionRow.addChild(replacementFunctionButton);
+                        (button, value) -> this.replacementFunction = value);
+        this.replacementFunctionButton.setWidth(width / 2 + DEFAULT_WIDTH + DEFAULT_SPACING);
+        this.replacementFunctionButton.setTooltip(Tooltip.create(Component.literal("On execution, the placeholder {function} will be replaced by this functions return value.")));
+        specialFunctionRow.addChild(this.replacementFunctionButton);
 
-        layout.visitWidgets(this::addRenderableWidget);
         layout.arrangeElements();
+        layout.visitWidgets(this::addRenderableWidget);
     }
 }
